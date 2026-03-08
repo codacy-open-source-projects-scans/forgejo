675991802e25eb787362bac5504cf789b67db948
Codacy Archive <codacy-archive@local>
2026-03-08 01:17:55 +0000

    snapshot 2026-03-08T01:17:55.903646+00:00

--- routers/api/packages/api.go
+++ routers/api/packages/api.go
@@ -118,6 +118,7 @@ func verifyAuth(r *web.Route, authMethods []auth.Method) {
 		ctx.Doer, err = authGroup.Verify(ctx.Req, ctx.Resp, ctx, ctx.Session)
 		if err != nil {
 			log.Info("Failed to verify user: %v", err)
+			container.APIUnauthorizedError(ctx)
 			ctx.Error(http.StatusUnauthorized, "authGroup.Verify")
 			return
 		}
@@ -353,11 +354,16 @@ func CommonRoutes() *web.Route {
 			})
 		}, reqPackageAccess(perm.AccessModeRead))
 		r.Group("/debian", func() {
-			r.Get("/repository.key", debian.GetRepositoryKey)
+			r.Group("/repository.key", func() {
+				r.Head("", debian.GetRepositoryKey)
+				r.Get("", debian.GetRepositoryKey)
+			})
 			r.Group("/dists/{distribution}", func() {
 				r.Get("/{filename}", debian.GetRepositoryFile)
+				r.Head("/{filename}", debian.CheckRepositoryFileExistence)
 				r.Get("/by-hash/{algorithm}/{hash}", debian.GetRepositoryFileByHash)
 				r.Group("/{component}/{architecture}", func() {
+					r.Head("/{filename}", debian.CheckRepositoryFileExistence)
 					r.Get("/{filename}", debian.GetRepositoryFile)
 					r.Get("/by-hash/{algorithm}/{hash}", debian.GetRepositoryFileByHash)
 				})
